5b906641454d5110e9590a01c3a9a7cab4f592f5
berlindiachica <rey24ti@mahasiswa.pcr.ac.id>
2025-10-09 11:10:55 +0700

    p5

---
+++ belajar-laravel/resources/views/halaman-about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h1>ini adalah halaman about</h1>
+    <p>laravel membuat development lebih cepat </p>
+</body>
+</html>
